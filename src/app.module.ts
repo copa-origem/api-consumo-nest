0a10f4fa63b1dcb182de44ac98e19c6fdf254045
Rafael Rangel <rafael.profissional011@gmail.com>
2025-12-15 19:55:02 -0300

    feat: creating the votes resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 import { ProblemsModule } from './problems/problems.module';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [PrismaModule, CategoriesModule, AuthModule, ProblemsModule],
+  imports: [PrismaModule, CategoriesModule, AuthModule, ProblemsModule, VotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
