f6ac40e3b58004fe33bf59927a783fd77cc96757
Rafael Rangel <rafael.profissional011@gmail.com>
2025-12-15 19:57:59 -0300

    feat: create vote dto config

--- src/votes/dto/create-vote.dto.ts
+++ src/votes/dto/create-vote.dto.ts
@@ -1,1 +1,10 @@
-export class CreateVoteDto {}
+import { IsEnum, IsUUID } from 'class-validator';
+import { VoteType } from '@prisma/client';
+
+export class CreateVoteDto {
+    @IsUUID()
+    problemId: string;
+
+    @IsEnum(VoteType)
+    type: VoteType;
+}
