01c60547753fa312437c66e5875a83e26f1b918f
Rafael Rangel <rafael.profissional011@gmail.com>
2025-12-16 19:21:45 -0300

    feat: add the swagger to main

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as admin from 'firebase-admin'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,8 +12,19 @@ async function bootstrap() {
   admin.initializeApp({
     credential: admin.credential.cert('./firebase-config.json'),
   });
-
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Alerta Cidadão API')
+    .setDescription('The API to manage the urban problems and comunity vote.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
