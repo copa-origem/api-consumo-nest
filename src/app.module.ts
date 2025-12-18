4d12a1c87132784719373048379cf1a80698a294
Rafael Rangel <rafael.profissional011@gmail.com>
2025-12-18 20:33:42 -0300

    feat: add joi verification to env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,25 @@ import { ProblemsModule } from './problems/problems.module';
 import { VotesModule } from './votes/votes.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [PrismaModule, CategoriesModule, AuthModule, ProblemsModule, ScheduleModule.forRoot(), VotesModule, CloudinaryModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        //firebase
+        FIREBASE_PROJECT_ID: Joi.string(),
+        //cloudinary
+        CLOUDINARY_NAME: Joi.string().required(),
+        CLOUDINARY_API_KEY: Joi.string().required(),
+        CLOUDINARY_API_SECRET: Joi.string().required(),
+        //port
+        PORT: Joi.number().default(3000),
+      }),
+    }), PrismaModule, CategoriesModule, AuthModule, ProblemsModule, ScheduleModule.forRoot(), VotesModule, CloudinaryModule
+  ]
 })
 export class AppModule {}
