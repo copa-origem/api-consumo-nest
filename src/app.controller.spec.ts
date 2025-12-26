8729ca1fb8824d194b798f414c9027c1d38d4359
Rafael Rangel <rafael.profissional011@gmail.com>
2025-12-25 21:14:37 -0300

    test: test 5 ci/cd

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World test! 4"', () => {
+    it('should return "Hello World test! 5"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
