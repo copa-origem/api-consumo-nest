9a97014867544dc6e0c890559031afdcf0957489
Rafael Rangel <rafael.profissional011@gmail.com>
2025-12-27 13:22:23 -0300

    feat: add new register to rabbit on problems module

--- src/problems/problems.module.ts
+++ src/problems/problems.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from "@nestjs/microservices";
 import { ProblemsService } from './problems.service';
 import { ProblemsController } from './problems.controller';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NOTIFICATIONS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://user:password@localhost:5672'],
+          queue: 'problems_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [ProblemsController],
   providers: [ProblemsService],
 })
