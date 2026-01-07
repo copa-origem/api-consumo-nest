0a95e5e4fca6c84d55d61b5af2a93bf3d18b841f
Rafael Rangel <rafael.profissional011@gmail.com>
2026-01-07 20:15:51 -0300

    feat: add the reports module

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,13 +2,26 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
+import { ReportsGateway } from './reports.gateway';
 
 @Module({
     imports: [
         ClientsModule.register([
             {
                 name: 'RABBITMQ_SERVICE',
-            }
-        ])
-    ]
+                transport: Transport.RMQ,
+                options: {
+                    urls: [process.env.RABBITMQ_URL || "amqp://user:password@localhost:5672"],
+                    queue: 'reports_queue',
+                    queueOptions: {
+                        durable: true,
+                    },
+                },
+            },
+        ]),
+    ],
+    controllers: [ReportsController],
+    providers: [ReportsService, ReportsGateway],
 })
+
+export class ReportsModule {}
