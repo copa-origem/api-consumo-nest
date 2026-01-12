30c89b0b26eac27a91015cfb2b7a7328692c85e6
Rafael Rangel <rafinhad3las@gmail.com>
2026-01-12 07:13:23 -0300

    feat: add the event pattern to the generate-report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -10,18 +10,20 @@ import {
 } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../auth/auth.guard';
-import { EventPattern, Payload, MessagePattern } from '@nestjs/microservices';
+import { EventPattern, Payload, MessagePattern, Ctx, RmqContext } from '@nestjs/microservices';
 import { NotificationsGateway } from '../notifications/notifications.gateway';
 import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-
-pdfMake.vfs = pdfFonts.pdfMake.vsf;
 
 const REPORTS_SERVICE = 'REPORTS_SERVICE';
 
 @Controller('reports')
 export class ReportsController {
-    constructor(private readonly ReportsService: ReportsService) {}
+    constructor(
+        private readonly reportsService: ReportsService,
+        private readonly notificationsGateway: NotificationsGateway,
+        @Inject('RABBITMQ_SERVICE') private readonly rabbitmqClient: any,
+    ) {}
 
     @Post('export')
     @UseGuards(AuthGuard)
@@ -30,6 +32,22 @@ export class ReportsController {
         const userId = req.user.id;
         const filters = body.filters;
 
-        return this.ReportsService.requestReport(userId, filters);
+        return this.reportsService.requestReport(userId, filters);
+    }
+
+    @EventPattern('gerenare-report')
+    async handleGenerateReport(@Payload() data: any, @Ctx() context: RmqContext) {
+        console.log('Worker recebeu job:', data.jobId);
+
+        const { userId, filters, jobId } = data;
+
+        const pdfUrl = await this.reportsService.generatePDF(filters);
+
+        console.log(`Warning the user ${userId}  by WebSocket...`);
+        this.notificationsGateway.notifyUser(userId, 'report_ready', {
+            message: 'Seu relatório está pronto!',
+            downloadUrl: pdfUrl,
+            jobId: jobId
+        });
     }
 }
