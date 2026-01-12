c298123084971b6051b1da40b3252d6be484c8c4
Rafael Rangel <rafinhad3las@gmail.com>
2026-01-12 10:16:56 -0300

    feat: add the new generate pdf to test

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -38,10 +38,64 @@ export class ReportsService {
             { titulo: 'Lixo na calçada', data: '10/01/2026', status: 'Aberto'},
         ];
 
-        await new Promise(resolve => setTimeout(resolve, 5000));
+        const fonts = {
+            Helvetica: {
+                normal: 'Helvetica',
+                bold: 'Helvetica-Bold',
+                italics: 'Helvetica-Oblique',
+                bolditalics: 'Helvetica-BoldOblique',
+            },
+        };
 
-        console.log('Pdf gerado');
+        const printer = new PdfPrinter(fonts);
 
-        return 'https://cloudinary.com/fake-url-do-relatorio.pdf';
+        const docDefinition: TDocumentDefinitions = {
+            defaultStyle: { font: 'Helvetica' },
+            content: [
+                { text: 'Relatório de Ocorrências Urbanas', style: 'header'},
+                { text: `Gerado em: ${new Date().toLocaleString()}`, style: 'subheader'},
+                { text: '\n\n'},
+
+                {
+                    table: {
+                        widths: ['*', 'auto', 'auto'],
+                        body: [
+                            [
+                                { text: 'Ocorrência', style: 'tableHeader' },
+                                { text: 'Data', style: 'tableHeader'},
+                                { text: 'Status', style: 'tableHeader' }
+                            ],
+                            ...denuncias.map(d => [d.titulo, d.data, d.status])
+                        ]
+                    }
+                }
+            ],
+            styles: {
+                header: {fontSize: 18, bold: true, alignment: 'center', margin: [0, 0, 0, 10] },
+                subheader: {fontSize: 12, italics: true, alignment: 'center' },
+                tableHeader: {bold: true, fontSize: 13, color: 'black', fillColor: '#eeeeee'}
+            }
+        };
+
+        return new Promise((resolve, reject) => {
+            const pdfDoc = printer.createPDFKitDocument(docDefinition);
+            const chunks: any[] = [];
+
+            pdfDoc.on('data', (chunk) => chunks.push(chunk));
+
+            pdfDoc.on('end', () => {
+                const result = Buffer.concat(chunks);
+
+                const base64Pdf = `data:application/pdf;base64,${result.toString('base64')}`;
+                console.log('pdf real gerado');
+                resolve(base64Pdf);
+            });
+
+            pdfDoc.on('error', (err) => {
+                reject(err);
+            });
+
+            pdfDoc.end();
+        });
     }
 }
